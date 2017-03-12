d64cf12a873faebc4fd64d6f7d56a2d8abac6dd8
Werner Kroneman <werner@wernerkroneman.nl>
2017-03-12 11:21:40 +0100

    Backtrack pattern

--- src/main/java/nl/wernerkroneman/Drawy/ParseTreeMatcher/PhrasePattern.kt
+++ src/main/java/nl/wernerkroneman/Drawy/ParseTreeMatcher/PhrasePattern.kt
@@ -123,133 +123,42 @@ class PhrasePattern(// Regexes for the word, nature and role
     }
 
     // Match the children with each other
-    private fun matchChildren(phrase: PhraseTree): MatchResult {
+    private fun matchChildren(phrase: PhraseTree,
+                              fromPhraseChildIndex: Int = 0,
+                              fromPatternChildIndex: Int = 0): MatchResult {
 
-        val phraseChildren = phrase.children!!
-        val patternChildren = this.children!!
+        val phrases = phrase.children!!.drop(fromPatternChildIndex)
+        val patterns = this.children!!.drop(fromPhraseChildIndex)
 
-        // If no children are allowed, check if this is the case
-        if (patternChildren.isEmpty()) {
-            if (phraseChildren.isEmpty()) {
-                return MatchResult(true,1.0)
-            } else {
-                return MatchResult(false)
-            }
-        }
+        val result = MatchResult(matches = false)
 
-        /**
-         * A 2-dimensional array of MatchResult objects.
-         *
-         * For some MatchResult r, resultTable[i][j] == r
-         * means that with pattern children up to and including
-         * this.children[j], r represents the best-possible way
-         * to match the phrase children up to and including
-         * phrase.children[i]
-         */
-        val resultTable = Array<Array<MatchResult>>(phraseChildren.size, {
-            Array(patternChildren.size, {
-                MatchResult(false,0.0,mutableMapOf())
-            })
-        })
-
-        // Iterate over the various cells in the table
-        // In both dimensions, in increasing order.
-        for (phraseIndex in 0 until phraseChildren.size) {
-            for (patternIndex in 0 until patternChildren.size) {
-
-                resultTable[phraseIndex][patternIndex] = computeOptimalResult(phraseChildren,
-                        phraseIndex,patternChildren,patternIndex,resultTable)
-            }
-        }
-
-        return resultTable.last().last()
-    }
-
-    /**
-     * Compute the best possible match, given phrases
-     * up to and including phrases[phraseIndex], and
-     * patterns up to and including patterns[patternIndex]
-     */
-    fun computeOptimalResult(phrases: List<PhraseTree>,
-                             phraseIndex: Int,
-                             patterns: List<PhrasePattern>,
-                             patternIndex: Int,
-                             resultTable: Array<Array<MatchResult>>) : MatchResult {
-
-        // Reference to current pattern child
-        var pattern = patterns[patternIndex]
-
-        /*
-         * Pattern "pattern" must match every phrase from a certain index
-         * matchFrom, with matchFromAtLeast <= matchFrom <= matchFromAtMost,
-         * with matchFrom chosen to be such that the total matchScore is
-         * maximal and matchFrom is 0, inclusive or (patternIndex > 0 and
-         * and resultTable[matchFrom][patternIndex-1].matches) is true.
-         */
-
-        if (patternIndex == 0) { // This is the first in the list of patterns
-            if (pattern.repeatMax != null && pattern.repeatMax!! < phraseIndex+1) {
-                // Cannot cover phrases[0]...phrases[patternIndex] by repeating
-                return MatchResult(matches = false)
-            } else if (pattern.repeatMin > phraseIndex+1) {
-                // phrases[0]...phrases[patternIndex] not enough to cover repeat count
-                return MatchResult(matches = false)
-            } else {
-                return matchAllPhrases(pattern, phrases.take(phraseIndex + 1))
-            }
-
-        } else { // There are previous patterns
-
-            // Determine last possible start position
-            val matchFromAtMost = phraseIndex - pattern.repeatMin+1
-            // Determine first possible start position
-            val matchFromAtLeast = if (pattern.repeatMax == null)
-                0
-            else
-                (phraseIndex - pattern.repeatMax!! + 1).coerceAtLeast(0)
-
-            // Range of all possible match start positions
-            return (matchFromAtLeast..matchFromAtMost)
-                    // Remove start positions that are not either the first prase
-                    // or for which there exists a sub-problem match
-                    // it-1 because we need to look back at a result of a previous phrase
-                    .filter({it == 0 || resultTable[it-1][patternIndex-1].matches})
-                    // Try to match with the fixed-length range
-                    .map {Pair(it,matchAllPhrases(pattern, phrases.subList(it,phraseIndex+1)))}
-                    // Filter out non-matches
-                    .filter {it.second.matches}
-                    // Merge the match results with the previous match
-                    .map {
-                        val result = MatchResult(it.second.matches,
-                                                 it.second.matchScore,
-                                                 HashMap(it.second.capturings))
-                        if (it.first >= 1) {
-                            result.merge(resultTable[it.first - 1][patternIndex - 1]!!)
-                        }
-                        Pair(it.first, result)}
-                    // Get the maximum by match score
-                    .maxBy { it.second.matchScore }
-                    // Return the result, or return non-match result if null.
-                    ?.second ?: MatchResult(false)
-
-        }
-    }
-
-    fun matchAllPhrases(pattern: PhrasePattern, phrases: List<PhraseTree>): MatchResult {
-        // Repeat count OK, all must match from 0, compute matches
-        val matches = phrases.map({ pattern.matchAgainst(it) })
-
-        if (matches.any { !it.matches }) {
-            // All must match
-            return MatchResult(matches = false)
-        } else {
-            // All match, accumulate match score and capturings
-            val result = MatchResult(matches = true)
-            for (match in matches) {
-                result.matchScore += match.matchScore
-                result.capturings.putAll(match.capturings)
-            }
+        if (patterns.isEmpty()) {
+            result.matches = phrases.isEmpty()
             return result
         }
+
+        val pattern = patterns.first()
+
+        if (phrases.size < pattern.repeatMin) {
+            result.matches = false
+            return result
+        }
+
+        val firstMatches = phrases.take(pattern.repeatMin)
+                                  .map {pattern.matchAgainst(it)}
+
+        if (firstMatches.any {!it.matches}) {
+            result.matches = false
+            return result
+        } else {
+            firstMatches.forEach { result.merge(it) }
+        }
+
+        pattern.repeatMin.until(
+                if (pattern.repeatMax == null) phrases.size
+                else pattern.repeatMax!!.coerceAtMost(phrases.size))
+            .map()
+
+        
     }
 }
